59395e93206399b561c253241b445db410017b04
LeonineKing1199 <christian.mazakas@gmail.com>
2018-06-11 13:05:12 -0700

    Got foxy working on OSX

---
+++ foxy-osx.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_BUILD_TYPE Debug)
+set(Boost_USE_STATIC_LIBS ON)
+set(CMAKE_C_COMPILER /Users/exbigboss/clang+llvm-6.0.0-x86_64-apple-darwin/bin/clang)
+set(CMAKE_CXX_COMPILER /Users/exbigboss/clang+llvm-6.0.0-x86_64-apple-darwin/bin/clang++)
+add_compile_options("-fcoroutines-ts")
+link_libraries("pthread")
+include("/Users/exbigboss/vcpkg/scripts/buildsystems/vcpkg.cmake")
+
