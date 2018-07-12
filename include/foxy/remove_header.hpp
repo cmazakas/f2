c5ae9ebbbbb4b8d740e2b6b41c9de36aae8ca3ae
LeonineKing1199 <christian.mazakas@gmail.com>
2018-07-11 18:36:02 -0700

    Added connection header filtering

--- include/foxy/remove_header.hpp
+++ include/foxy/remove_header.hpp
@@ -5,35 +5,38 @@
 #include <boost/beast/http/field.hpp>
 #include <boost/beast/http/type_traits.hpp>
 
+#include <boost/range/algorithm/for_each.hpp>
+
 #include <vector>
 #include <string>
-#include <algorithm>
 
 namespace foxy {
 
 template <typename Fields>
 auto remove_connection_header(Fields& fields) {
-  namespace x3   = boost::spirit::x3;
-  namespace http = boost::beast::http;
 
-  auto const field = http::field::connection;
-
-  auto [begin, end] = fields.equal_range(field);
+  namespace x3    = boost::spirit::x3;
+  namespace http  = boost::beast::http;
+  namespace range = boost::range;
 
   auto tokens = std::vector<std::string>();
 
-  std::for_each(begin, end, [&](auto const& field) mutable {
-    auto const val = fields[field.name()];
-    if (val == "") { return; }
+  range::for_each(
+    fields.equal_range(http::field::connection),
+    [&](auto const& field) {
+      auto const val = field.value();
+      if (val == "") { return; }
 
-    auto const token = +(x3::char_ - ',');
+      auto const token = +(x3::char_ - ',');
 
-    x3::phrase_parse(
-      val.begin(), val.end(),
-      *x3::lit(',') >> token >> *(',' >> token),
-      x3::ascii::space,
-      tokens);
-  });
+      x3::phrase_parse(
+        val.begin(), val.end(),
+        *x3::lit(',') >> token >> *(',' >> token),
+        x3::ascii::space,
+        tokens);
+    });
+
+  range::for_each(tokens, [&](auto const& t) { fields.erase(t); });
 }
 
 } // foxy
--- test/remove_header_test.cpp
+++ test/remove_header_test.cpp
@@ -1,4 +1,3 @@
-
 #include "foxy/remove_header.hpp"
 #include <boost/beast/http.hpp>
 #include <catch/catch.hpp>
@@ -7,10 +6,20 @@ namespace http = boost::beast::http;
 
 TEST_CASE("Our connection header removal function") {
   SECTION("should do as advertised") {
+
     auto fields = http::fields();
-    fields.set(http::field::connection, "lol");
-    fields.set(http::field::connection, "foo");
+    fields.insert(http::field::connection, "lol");
+    fields.insert(http::field::connection, "foo");
+
+    fields.insert("lol", "muahahaha");
+    fields.insert("lol", "and another one too!");
+
+    fields.insert("foo", "we'll only test one instance of this field");
 
     foxy::remove_connection_header(fields);
+
+    REQUIRE(fields["lol"] == "");
+    REQUIRE(fields["foo"] == "");
+    REQUIRE(fields[http::field::connection] != "");
   }
 }
