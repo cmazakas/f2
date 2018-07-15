b2cacb5be852a72b89fd861e3a86852140469ba4
LeonineKing1199 <christian.mazakas@gmail.com>
2018-07-15 15:54:28 -0700

    Improved test coverage over Connection partitioning

--- test/remove_header_test.cpp
+++ test/remove_header_test.cpp
@@ -31,4 +31,29 @@ TEST_CASE("Our connection header removal function") {
 
     CHECK(proxy_fields["lol"] == "lol-tastic value");
   }
+
+  SECTION("should support a variation of the Connection value") {
+
+    auto const* connection_options = ", , ,, , , ,,upgrade , lol";
+
+    auto fields = http::fields();
+    fields.insert(http::field::connection, connection_options);
+    fields.insert(http::field::upgrade, "h2c, HTTPS/1.3, IRC/6.9, RTA/x11");
+    fields.insert("lol", "lol-tastic value");
+
+    auto proxy_fields = http::fields();
+
+    foxy::partition_connection_options(fields, proxy_fields);
+
+    CHECK(fields[http::field::connection] == "");
+    CHECK(fields[http::field::upgrade]    == "");
+    CHECK(fields["lol"]                   == "");
+
+    CHECK(proxy_fields[http::field::connection] == connection_options);
+    CHECK(
+      proxy_fields[http::field::upgrade] ==
+      "h2c, HTTPS/1.3, IRC/6.9, RTA/x11");
+
+    CHECK(proxy_fields["lol"] == "lol-tastic value");
+  }
 }
